9c668d1301f795308d6af3fe4f3cc7aafd375092
Kevinmsouza <kevinm.souza@hotmail.com>
2021-11-19 03:17:48 -0400

    fixed environment variable

--- src/services/gratibox.js
+++ src/services/gratibox.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_URL === "prod" ? "https://kms-gratibox.herokuapp.com" : "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? "https://kms-gratibox.herokuapp.com" : "http://localhost:4000";
